ea9a1f9d9910d66b43cc21df61f64f981e2f3ae4
canbax <yusufsaidcanbaz@gmail.com>
2026-01-01 00:22:43 +0300

    simpler UI

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -1,12 +1,12 @@
 import { Excalidraw } from "@excalidraw/excalidraw";
 
 export default function Whiteboard() {
-  return (
-    <div style={{ height: "100vh", width: "100%", display: "flex", flexDirection: "column" }}>
-      <h1 style={{ textAlign: "center", padding: "10px", margin: 0, backgroundColor: "#f0f0f0" }}>Mock Whiteboard</h1>
-      <div style={{ flex: 1 }}>
-        <Excalidraw />
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
+            <div style={{ height: "500px" }}>
+                <Excalidraw />
+            </div>
+        </>
+    );
 }
