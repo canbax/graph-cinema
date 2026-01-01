833e0f86da8612189a77b2295519d1af4e77656e
canbax <yusufsaidcanbaz@gmail.com>
2026-01-01 19:36:24 +0300

    import excalidraw CSS to make it render properly

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -1,12 +1,12 @@
 import { Excalidraw } from "@excalidraw/excalidraw";
 
 export default function Whiteboard() {
-    return (
-        <>
-            <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
-            <div style={{ height: "500px" }}>
-                <Excalidraw />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
+      <div style={{ height: "800px" }}>
+        <Excalidraw />
+      </div>
+    </>
+  );
 }
