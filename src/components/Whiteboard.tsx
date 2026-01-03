471c5335674b2a8e07ddd1f2f6f08c89bb943df2
canbax <yusufsaidcanbaz@gmail.com>
2026-01-03 14:13:04 +0300

    remove excalidraw header

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -3,7 +3,6 @@ import { Excalidraw } from "@excalidraw/excalidraw";
 export default function Whiteboard() {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
       <div style={{ height: "500px" }}>
         <Excalidraw />
       </div>
